62fd61e73cb9b14aa099e7e91aa651968ac6847a
maxenceleguery <maxence.leguery@gmail.com>
2023-05-24 14:43:30 +0200

    initial commit

---
+++ script.js
@@ -0,0 +1,28 @@
+const header = document.querySelector('.navbar');
+
+window.onscroll = function() {
+    var top = window.scrollY;
+    if(top >=100) {
+        header.classList.add('navbarDark');
+    }
+    else {
+        header.classList.remove('navbarDark');
+    }
+}
+
+function getAge(dateString) {
+    var today = new Date();
+    var birthDate = new Date(dateString);
+    var age = today.getFullYear() - birthDate.getFullYear();
+    var m = today.getMonth() - birthDate.getMonth();
+    if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
+        age--;
+    }
+    return age;
+}
+
+window.onload = function() {
+    var age = getAge('2001-09-23');
+    var ageText = document.getElementById("age");
+    ageText.innerHTML = "Age : "+age;
+}
