d919d52b3929fe6e6d8b19f12343db44fc04cb88
Maxence Leguéry <maxence.leguery@gmail.com>
2024-11-26 13:38:05 +0100

    Update

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const header = document.querySelector('.navbar');
 
-window.onscroll = function() {
-    var top = window.scrollY;
-    if(top >=100) {
+window.onscroll = function () {
+    let top = window.scrollY;
+    if (top >= 100) {
         header.classList.add('navbarDark');
     }
     else {
@@ -11,18 +11,32 @@ window.onscroll = function() {
 }
 
 function getAge(dateString) {
-    var today = new Date();
-    var birthDate = new Date(dateString);
-    var age = today.getFullYear() - birthDate.getFullYear();
-    var m = today.getMonth() - birthDate.getMonth();
+    let today = new Date();
+    let birthDate = new Date(dateString);
+    let age = today.getFullYear() - birthDate.getFullYear();
+    let m = today.getMonth() - birthDate.getMonth();
     if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
         age--;
     }
     return age;
 }
 
-window.onload = function() {
-    var age = getAge('2001-09-23');
-    var ageText = document.getElementById("age");
-    ageText.innerHTML = "Age : "+age;
+let acc = document.getElementsByClassName("accordion");
+
+for (let i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function () {
+        this.classList.toggle("active");
+        let panel = this.nextElementSibling;
+        if (panel.style.display === "block") {
+            panel.style.display = "none";
+        } else {
+            panel.style.display = "block";
+        }
+    });
+}
+
+window.onload = function () {
+    let age = getAge('2001-09-23');
+    let ageText = document.getElementById("age");
+    ageText.innerHTML = "Age : " + age;
 }
